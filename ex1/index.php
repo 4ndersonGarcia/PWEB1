89857d70cf3d153e735e543e1b86ec0d363ca8f9
4ndersonGarcia <60760402+4ndersonGarcia@users.noreply.github.com>
2020-02-06 20:30:04 -0300

    Add files via upload

---
+++ ex1/index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Exercicio 1</title>
+</head>
+<body>
+	<form action="divisor.php" method="get">
+		Digite um numero<input type="text" name="numero">
+	</form>
+</body>
+</html>
